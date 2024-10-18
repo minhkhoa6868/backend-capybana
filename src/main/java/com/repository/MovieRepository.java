1987ceb9d51d500614816fd8a6814e54ed12a5c3
KhoaAnhNguyen04 <khoanguyenanh040104@gmail.com>
2024-10-18 17:50:10 +0700

    update Api Message annotation + updated movie data

--- src/main/java/com/repository/MovieRepository.java
+++ src/main/java/com/repository/MovieRepository.java
@@ -3,7 +3,6 @@ package com.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.model.Movie;
-import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
