0676b99ae4890769d21a3b3f77d7fa99fd9a4cf5
Hatcodechet <130855069+Hatcodechet@users.noreply.github.com>
2024-11-22 22:53:28 +0700

    Revert "upload file"

--- src/main/java/com/service/WishlistService.java
+++ src/main/java/com/service/WishlistService.java
@@ -1,5 +1,7 @@
 package com.service;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
