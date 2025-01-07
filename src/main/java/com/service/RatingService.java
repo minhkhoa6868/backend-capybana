bdcb8115bb959e959a5a8e1da69d571a799d84cd
Hatcodechet <130855069+Hatcodechet@users.noreply.github.com>
2025-01-07 15:55:39 +0700

    add averageRating (#33)

--- src/main/java/com/service/RatingService.java
+++ src/main/java/com/service/RatingService.java
@@ -1,7 +1,9 @@
 
 package com.service;
 
+import com.model.Movie;
 import com.model.Rating;
+import com.repository.MovieRepository;
 import com.repository.RatingRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,13 +16,45 @@ public class RatingService {
 
     private final RatingRepository ratingRepository;
 
-    public RatingService(RatingRepository ratingRepository) {
+    //add movieRepository
+     private final MovieRepository movieRepository; 
+
+    public RatingService(RatingRepository ratingRepository, MovieRepository movieRepository) {
         this.ratingRepository = ratingRepository;
+        this.movieRepository = movieRepository;
     }
 
     public Rating createRating(Rating rating) {
         rating.setRatingDate(LocalDateTime.now());
-        return ratingRepository.save(rating);
+        Rating newRating = ratingRepository.save(rating);
+
+        //calculate average
+        Long movieId = newRating.getMovie().getId();
+        float newAvg = this.calculateAverageRating(movieId);
+
+        Optional<Movie> optMovie = movieRepository.findById(movieId);
+        if (optMovie.isPresent()) {
+            Movie movie = optMovie.get();
+            movie.setMovieRating(newAvg);
+            movieRepository.save(movie);
+        }
+        
+        return newRating;
+
+
+    }
+    /*Function: calculate average Rating */
+    public float calculateAverageRating(Long movieId){
+        List<Rating> listRating = ratingRepository.findByMovieId(movieId);
+
+        if(listRating.isEmpty()){
+            return 0;
+        }
+        float sum = 0;
+        for(Rating r : listRating){
+            sum+=r.getRating();
+        }
+        return sum/listRating.size();
     }
 
     // truy van 1 danh gia dua vao ID
